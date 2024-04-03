8d2ef76c3923afffc5b5e0557532a01dfeafe390
SMIT BHALANI <smitbhalani@192.168.2.7>
2024-04-03 22:04:07 +0530

    first commit

---
+++ app/_utils/GlobalAPI.jsx
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+
+const axiosClient = axios.create({
+    baseURL: "http://localhost:1337/api"
+})
+
+const getCategory = () => axiosClient.get('/categories?populate=*')
+const getDoctorList=()=>axiosClient.get('/doctors?populate=*')
+const getDoctorByCategory=(category)=>axiosClient.get('/doctors?filters[categories][Name][$in]='+category+"&populate=*")
+const getDoctorById=(id)=>axiosClient.get('/doctors/'+id+"?populate=*")
+
+ const bookAppointment=(data)=>axiosClient.post('/appointments',data);
+ 
+    const getUserBookingList=(userEmail)=>axiosClient.get("/appointments?[filters][Email][$eq]="+userEmail+"&populate[doctor][populate][image][populate][0]=url&populate=*")
+
+    const deleteBooking=(id)=>axiosClient.delete('/appointments/'+id)
+ 
+    const sendEmail=(data)=>axios.post('/api/sendEmail',data);
+    export default{
+    getCategory,
+    getDoctorList,
+    getDoctorByCategory,
+    getDoctorById,
+    bookAppointment,
+    getUserBookingList,
+    deleteBooking,
+    sendEmail
+    }
